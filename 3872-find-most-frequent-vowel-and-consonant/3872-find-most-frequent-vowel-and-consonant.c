f4dbca33553916603d81f1ccf2bf9a2b67280243
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2025-09-13 17:21:38 +0530

    Time: 0 ms (100%), Space: 9.5 MB (6.12%) - LeetHub

---
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.c
@@ -0,0 +1,40 @@
+bool isvowel(char ch){
+    switch(ch){
+        case 'a':
+        case 'A':
+        case 'e':
+        case 'E':
+        case 'i':
+        case 'I':
+        case 'o':
+        case 'O':
+        case 'u':
+        case 'U':return true;
+        default:return false;
+    }
+}
+
+bool iscons(char ch){
+    if(isvowel(ch))return false;
+    return true;
+}
+
+int vowelmax(int *k){
+    int max=0;
+    for(int i=0;i<256;i++) if(isvowel((char)i))if(k[i]>=max)max=k[i];
+    return max;
+}
+
+int consmax(int *k){
+    int max=0;
+    for(int i=0;i<256;i++)if(iscons((char)i)) if(k[i]>=max)max=k[i];
+    return max;
+}
+
+int maxFreqSum(char* s) {
+    int count[256]={0};
+    for(int i=0;s[i]!='\0';i++)count[s[i]]++;
+    int vm=vowelmax(count);
+    int cm=consmax(count);
+    return vm+cm;
+}
