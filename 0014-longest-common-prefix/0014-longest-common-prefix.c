81cfb1f7431919567b694223b7cc15f5b5ff0b22
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2025-09-17 18:37:45 +0530

    Time: 0 ms (100%), Space: 8.3 MB (9.3%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -0,0 +1,19 @@
+char * cpm(char *a,char * b){
+    int t=(strlen(a)>strlen(b))?strlen(a):strlen(b);
+    char *ans=(char *)malloc(t+1);
+    int c=0;
+    for(int i=0;a[i]!='\0'&&b[i]!='\0';i++){
+        if(a[i]==b[i])ans[c++]=a[i];
+        else break;
+    }
+    ans[c]='\0';
+    return ans;
+}
+
+char* longestCommonPrefix(char** strs, int strsSize) {
+    char *ans=strs[0];
+    for(int i=0;i<strsSize-1;i++){
+        ans=cpm(ans,strs[i+1]);
+    }
+    return ans;
+}
