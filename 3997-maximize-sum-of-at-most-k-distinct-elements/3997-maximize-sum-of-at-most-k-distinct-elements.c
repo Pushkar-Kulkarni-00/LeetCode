b1db778c799f52a897f2e5218e26354aa97e33d7
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2025-09-20 17:47:17 +0530

    Time: 0 ms (100%), Space: 13.3 MB (88.45%) - LeetHub

--- 3997-maximize-sum-of-at-most-k-distinct-elements/3997-maximize-sum-of-at-most-k-distinct-elements.c
+++ 3997-maximize-sum-of-at-most-k-distinct-elements/3997-maximize-sum-of-at-most-k-distinct-elements.c
@@ -1,35 +1,23 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-
-int * sort(int *k,int c){
-    int r=0;
-    for(int i=0;i<c-1;i++){
-        for(int j=i+1;j<c;j++){
-            if(k[j]>k[i])k[i]=k[i]+k[j]-(k[j]=k[i]);
-        }
-    }
-    return k;
-}
-int* rd(int *k,int c,int *l){
-    for(int i=0;i<c;i++){
-        for(int j=0;j<c;j++){
-            if(k[i]==k[j]&&i!=j)k[j]=0;
-        }
-    }
-    int *ans=(int *)malloc(sizeof(int)*c);
-    int r=0;
-    for(int i=0;i<c;i++){
-        if(k[i])ans[r++]=k[i];
-    }
-    *l=r;
-    return ans;
+int cmp(const void *a,const void *b){
+    int x = *(const int *)a;
+    int y = *(const int *)b;
+    return (y>x)-(y<x);
 }
 
 int* maxKDistinct(int* nums, int numsSize, int k, int* returnSize) {
-    nums=sort(nums,numsSize);
-    int r=0;
-    nums=rd(nums,numsSize,&r);
-    *returnSize=k>r?r:k;
-    return nums;
+    qsort(nums,numsSize,sizeof(int),cmp);
+    int *out = malloc(sizeof(int) * (k < numsSize ? k : numsSize));
+    int count = 0;
+
+    for (int i = 0; i < numsSize && count < k; i++) {
+        if (i == 0 || nums[i] != nums[i - 1]) {
+            out[count++] = nums[i];
+        }
+    }
+
+    *returnSize = count;
+    return out;
 }
