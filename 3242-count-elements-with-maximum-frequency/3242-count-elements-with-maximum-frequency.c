512acd99488b25d1ff01f0265c4c1f2b276b6493
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2025-09-22 12:18:18 +0530

    Time: 0 ms (100%), Space: 9.3 MB (34.04%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.c
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.c
@@ -3,7 +3,7 @@ int maxFrequencyElements(int* nums, int numsSize) {
     for(int i=0;i<numsSize;i++)k[nums[i]-1]++;
     int max=k[0],n=0;
     for(int i=0;i<100;i++){
-        if(k[i]>max){max=k[i];n=0;}
+        if(k[i]>max){max=k[i];n=0;};
         if(k[i]==max)n++;
     }
     return max*n;
