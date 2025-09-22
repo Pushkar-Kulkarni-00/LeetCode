831ba6f12b371c0256177f17cc26a63f29d8cffe
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2025-09-22 14:24:16 +0530

    Time: 0 ms (100%), Space: 8 MB (77.63%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.c
+++ 0058-length-of-last-word/0058-length-of-last-word.c
@@ -1,20 +1,10 @@
 int lengthOfLastWord(char* s) {
-    int k=strlen(s);
-    char *w =(char *)malloc(k+1);
-    char *pw =(char *)malloc(k+1);
-    int c=0;
-    for(int i=0;s[i]!='\0';i++){
-        if(s[i]==' '){
-            w[c]='\0';
-            if(w[0]!='\0')strcpy(pw,w);
-            c=0;
-        }
-        else{
-            w[c++]=s[i];
-        }
+    int i=strlen(s)-1;
+    while(i>=0 && s[i]==' ')i--;
+    int count =0;
+    while(i>=0 && s[i]!=' '){
+        count++;
+        i--;
     }
-    
-    if(c==0)strcpy(w,pw);
-    else w[c]='\0';
-    return strlen(w);
+    return count;
 }
