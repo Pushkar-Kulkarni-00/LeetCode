141d9df8a97feb889094428d89530efb984a4a87
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2026-03-05 17:35:50 +0530

    Time: 0 ms (100%), Space: 13.4 MB (6.97%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.c
+++ 0002-add-two-numbers/0002-add-two-numbers.c
@@ -1,36 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-    struct ListNode *head=NULL, *tail=NULL;
-    int c=0;
-
-    while(l1!=NULL || l2!=NULL){
-        int v1 = (l1 ? l1->val : 0);
-        int v2 = (l2 ? l2->val : 0);
-
-        int sum = v1 + v2 + c;
-        c = sum / 10;
-
-        struct ListNode *nn = (struct ListNode*)malloc(sizeof(struct ListNode));
-        nn->val = sum % 10;
-        nn->next = NULL;
-
-        if(head==NULL){  
+    int carry=0;
+    struct ListNode*head=NULL;
+    struct ListNode*tail=NULL;
+    while(l1!=NULL || l2!=NULL || carry){
+        int t=carry;
+        if(l1!=NULL){t+=l1->val;l1=l1->next;}
+        if(l2!=NULL){t+=l2->val;l2=l2->next;}
+        struct ListNode * nn=(struct ListNode*)malloc(sizeof(struct ListNode));
+        nn->val=t%10;
+        carry=t/10;
+        nn->next=NULL;
+        if (head==NULL){
             head=nn;
             tail=nn;
-        }else{           
+        }
+        else{
             tail->next=nn;
             tail=nn;
         }
-
-        if(l1) l1=l1->next;
-        if(l2) l2=l2->next;
     }
-
-    if(c>0){   
-        struct ListNode *nn=(struct ListNode*)malloc(sizeof(struct ListNode));
-        nn->val=c;
-        nn->next=NULL;
-        tail->next=nn;
-    }
-
     return head;
 }
