8b9e0a1d89bbff908f55eabae4ee2e7bb541559d
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2026-05-02 20:08:07 +0530

    Time: 0 ms (100%), Space: 8.5 MB (64.29%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.c
+++ 0788-rotated-digits/0788-rotated-digits.c
@@ -1,24 +1,30 @@
 int rotatedDigits(int n) {
-    int cnt=0;
-    for(int i=1;i<=n;i++){
-        int x=i;
-        int inv=0;
-        int r=0;
-        int t=0;
-        while(x>0){
-            if(x%10==5)r=r+(2)*pow(10,t++);
-            else if(x%10==2)r=r+(5)*pow(10,t++);
-            else if(x%10==6)r=r+(9)*pow(10,t++);
-            else if(x%10==9)r=r+(6)*pow(10,t++);
-            else if (x%10==0||x%10==1||x%10==8)r=r+(x%10)*pow(10,t++);
-            else {
-                inv=1;
+    int cnt = 0;
+
+    for (int i = 1; i <= n; i++) {
+        int x = i;
+        int valid = 1;
+        int changed = 0;
+
+        while (x > 0) {
+            int d = x % 10;
+
+            if (d == 3 || d == 4 || d == 7) {
+                valid = 0;
                 break;
             }
-            x/=10;
+
+            if (d == 2 || d == 5 || d == 6 || d == 9) {
+                changed = 1;
+            }
+
+            x /= 10;
         }
-        if(inv==1)continue;
-        if(r!=i)cnt++;
+
+        if (valid && changed) {
+            cnt++;
+        }
     }
+
     return cnt;
 }
