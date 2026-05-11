fedf54e604e0331ced31d22f19b6a9cc54161fed
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2026-05-11 16:52:27 +0530

    Time: 0 ms (100%), Space: 14.7 MB (32%) - LeetHub

---
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.c
@@ -0,0 +1,25 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* separateDigits(int* nums, int numsSize, int* returnSize) {
+    int *ans=(int *)malloc(sizeof(int)*100000000);
+    int t=0;
+    for(int i=numsSize-1;i>=0;i--){
+        int x=nums[i];
+        while(x>0){
+            ans[t++]=x%10;
+            x/=10;
+        }
+    }
+    *returnSize=t;
+    int l=0;
+    int r=t-1;
+    while(l<r){
+        int temp=ans[l];
+        ans[l]=ans[r];
+        ans[r]=temp;
+        l++;
+        r--;
+    }
+    return ans;
+}
