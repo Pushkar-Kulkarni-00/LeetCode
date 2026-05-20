920550c8d94587e3adaf01c5c7ff9c4c523a95a2
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2026-05-20 11:12:49 +0530

    Time: 0 ms (100%), Space: 14.9 MB (85.71%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.c
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.c
@@ -6,20 +6,15 @@ int* findThePrefixCommonArray(int* A, int ASize, int* B, int BSize, int* returnS
     int n=ASize;
     int fa[n+1];
     int fb[n+1];
-    for(int i=0;i<n+1;i++)fa[i]=0;
-    for(int i=0;i<n+1;i++)fb[i]=0;
+    for(int i=0;i<n+1;i++)fa[i]=fb[i]=0;
     int *ans=(int *)malloc(sizeof(int)*(n+1));
-    fa[A[0]]=1;
-    fb[B[0]]=1;
-    ans[n]=n;
-    ans[0]=A[0]==B[0]?1:0;
-    for(int i=1;i<n;i++){
+    int c=0;
+    for(int i=0;i<n;i++){
         fa[A[i]]=1;
         fb[B[i]]=1;
-        int c=0;
-        for(int j=i;j>=0;j--){
-            if(fa[B[j]]==1)c++;
-        }
+        if(fb[A[i]]==1)c++;
+        if(fa[B[i]]==1)c++;
+        if(A[i]==B[i])c--;
         ans[i]=c;
     }
     return ans;
