6f32c9bd9fb519340eb786ac2318bc4e49021120
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2026-05-27 08:26:31 +0530

    Time: 21 ms (63.22%), Space: 13.6 MB (37.93%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.c
@@ -0,0 +1,22 @@
+int numberOfSpecialChars(char* word) {
+    int chk[26]={0};
+    int chek[26]={0};
+    int db[26]={0};
+    for(int i=0;word[i]!='\0';i++){
+        char s=word[i];
+        if(s>='a' && s<='z'){
+            chk[s-'a']++;
+            if(chek[s-'a']>0){
+                db[s-'a']=1;
+            }
+        }
+        if(s>='A' && s<='Z'){
+            chek[s-'A']++;
+        }
+    }
+    int cnt=0;
+    for(int i=0;i<26;i++){
+        if(chk[i]>0 && chek[i]>0 &&db[i]==0)cnt++;
+    }
+    return cnt;
+}
