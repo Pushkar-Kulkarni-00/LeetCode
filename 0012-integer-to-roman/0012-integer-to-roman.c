2832cd0e56db3f6b54374bd03dd62adf6e9784bb
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2026-06-01 12:50:56 +0530

    Time: 3 ms (34.76%), Space: 10 MB (32.09%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.c
+++ 0012-integer-to-roman/0012-integer-to-roman.c
@@ -1,78 +1,16 @@
 char* intToRoman(int num) {
-    char *ans=(char *)malloc(sizeof(char)*100000);
-    int c=0;
-    while(num>0){
-        if(num>=1000){
-            ans[c++]='M';
-            num-=1000;
-        }
-        else{
-            if(num>=900){
-                num-=900;
-                ans[c++]='C';
-                ans[c++]='M';
-            }
-            else if(num>=500){
-                ans[c++]='D';
-                num-=500;
-            }
-            else{
-                if(num>=400){
-                    num-=400;
-                    ans[c++]='C';
-                    ans[c++]='D';
-                }
-                else if(num>=100){
-                    ans[c++]='C';
-                    num-=100;
-                }
-                else{
-                    if(num>=90){
-                        num-=90;
-                        ans[c++]='X';
-                        ans[c++]='C';    
-                    }
-                    else if(num>=50){
-                        num-=50;
-                        ans[c++]='L';
-                    }
-                    else{
-                        if(num>=40){
-                            num-=40;
-                            ans[c++]='X';
-                            ans[c++]='L';
-                        }
-                        else if (num>=10){
-                            num-=10;
-                            ans[c++]='X';
-                        }
-                        else{
-                            if(num>=9){
-                                num-=9;
-                                ans[c++]='I';
-                                ans[c++]='X';
-                            }
-                            else if(num>=5){
-                                num-=5;
-                                ans[c++]='V';
-                            }
-                            else{
-                                if(num>=4){
-                                    num-=4;
-                                    ans[c++]='I';
-                                    ans[c++]='V';
-                                }
-                                else {
-                                    num-=1;
-                                    ans[c++]='I';
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    ans[c]='\0';
+    char *ans = malloc(20);
+
+    char *thousands[] = {"", "M", "MM", "MMM"};
+    char *hundreds[]  = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    char *tens[]      = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    char *ones[]      = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+    sprintf(ans, "%s%s%s%s",
+            thousands[num / 1000],
+            hundreds[(num % 1000) / 100],
+            tens[(num % 100) / 10],
+            ones[num % 10]);
+
     return ans;
 }
