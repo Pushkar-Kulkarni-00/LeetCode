fa24b2b372733ffefb5fb4c1ef7b01a9fa211c3d
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2026-06-13 19:35:20 +0530

    Time: 0 ms (100%), Space: 11 MB (36.11%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.c
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.c
@@ -3,9 +3,7 @@ char* mapWordWeights(char** words, int wordsSize, int* weights, int weightsSize)
     int t=0;
     for(int i=0;i<wordsSize;i++){
         int s=0;
-        for(int j=0;words[i][j]!='\0';j++){
-            s+=weights[words[i][j]-'a'];
-        }
+        for(int j=0;words[i][j]!='\0';j++)s+=weights[words[i][j]-'a'];
         word[t++]=(26-s%26-1)+'a';
     }
     word[t++]='\0';
