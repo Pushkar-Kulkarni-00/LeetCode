ff47cc6f911c03ada17d7f83beb731945caae237
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2026-06-15 02:00:53 +0530

    Time: 2 ms (58.21%), Space: 51.1 MB (40.8%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.c
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.c
@@ -5,26 +5,29 @@
  *     struct ListNode *next;
  * };
  */
-
-int st[100000];
-int t;
-
+int stack[100001];
+int t=-1;
+int pop(){
+    return stack[t--];
+}
 void push(int x){
-    st[t++]=x;
+    stack[++t]=x;
 }
 
 int pairSum(struct ListNode* head) {
-    t=0;
-    struct ListNode * temp=head;
-    while(temp!=NULL){
-        push(temp->val);
-        temp=temp->next;
+    struct ListNode *i=head;
+    struct ListNode *j=head;
+    while(j!=NULL){
+        push(i->val);
+        i=i->next;
+        j=j->next->next;
     }
     int max=0;
-    int s;
-    for(int i=0;i<t/2;i++){
-        s=st[i]+st[t-i-1];
-        if(s>max)max=s;
+    while(i!=NULL){
+        int lmax=pop();
+        lmax+=i->val;
+        if(lmax>max)max=lmax;
+        i=i->next;
     }
     return max;
 }
