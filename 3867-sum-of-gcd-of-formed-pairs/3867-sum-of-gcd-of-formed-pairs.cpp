043d2c6ef1b5bd00b3ff0a06e9acd11ad860842d
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2026-07-16 23:56:12 +0530

    Time: 105 ms (5.53%), Space: 203.8 MB (7.22%) - LeetHub

---
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long gcd(long long m,long long k){
+        if(k==0)return m;
+        return gcd(k,m%k);
+    }
+
+    long long gcdSum(vector<int>& nums) {
+        vector<long long> prefixGcd;
+        vector <long long> mx;
+        long long max=-1;
+        for(long long i:nums){
+            if(i>max)max=i;
+            mx.emplace_back(max);
+        }
+        for(long long i=0;i<nums.size();i++){
+            prefixGcd.emplace_back(gcd(nums[i],mx[i]));
+        }
+        sort(prefixGcd.begin(),prefixGcd.end());
+        long long ans=0;
+        int r=prefixGcd.size()-1;
+        int l=0;
+        while(r>l){
+            ans+=gcd(prefixGcd[r],prefixGcd[l]);
+            r--;
+            l++;
+        }
+        return ans;
+    }
+};
