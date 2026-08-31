ec0c84063108de25e5a96f5ffc4c8f718ec7c385
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2026-08-31 22:47:39 +0530

    Time: 1 ms (71.57%), Space: 10.8 MB (13.23%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if(num1=="0" || num2=="0")return "0";
+
+        int n=num1.size();
+        int m=num2.size();
+
+        vector <int> ans(n+m,0);
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                ans[i+j+1]+=(num1[i]-'0')*(num2[j]-'0');
+                ans[i+j]+=ans[i+j+1]/10;
+                ans[i+j+1]=ans[i+j+1]%10;
+            }
+        }
+        string a="";
+        int i=0;
+        while(ans[i]==0)i++;
+        while(i<n+m)a+=to_string(ans[i++]);
+
+        return a;
+    }
+};
