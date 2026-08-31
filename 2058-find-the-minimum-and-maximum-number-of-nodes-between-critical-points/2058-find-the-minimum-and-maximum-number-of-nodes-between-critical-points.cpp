ebef39efa5f9452f60e044b60318e548ea8b2182
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2026-08-31 11:24:30 +0530

    Time: 7 ms (33.43%), Space: 134.1 MB (5.02%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,82 +1,51 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        vector <int> ans;
-        if(head->next->next==NULL){
-            ans.emplace_back(-1);
-            ans.emplace_back(-1);
-            return ans;
-        }
-        ListNode *p=NULL;
-        ListNode *c=head;
-        ListNode *n=c->next;
+        vector<int> ans;
 
-        vector <int> chk;
+        if (head == nullptr || head->next == nullptr || head->next->next == nullptr)
+            return {-1, -1};
 
-        while(c!=NULL){
-            if(p==NULL ||n==NULL){
-                chk.emplace_back(0);
+        ListNode* p = head;
+        ListNode* c = head->next;
+        ListNode* n = c->next;
 
-                p=c;
-                c=n;
-                if(n)n=n->next;
-                continue;
+        vector<int> chk;
+
+        while (n != nullptr) {
+            if ((p->val > c->val && n->val > c->val) ||
+                (p->val < c->val && n->val < c->val)) {
+                chk.push_back(1);
+            } else {
+                chk.push_back(0);
             }
 
-            if((p->val>c->val && n->val>c->val)||(p->val<c->val&&n->val<c->val))chk.emplace_back(1);
-            else chk.emplace_back(0);
-            p=c;
-            c=n;
-            n=n->next;
-        }
-        int t=0;
-        for(int x:chk)if(x)t++;
-        if(t<2){
-            ans.emplace_back(-1);
-            ans.emplace_back(-1);
-            return ans;    
+            p = c;
+            c = n;
+            n = n->next;
         }
 
-        int l=-1;
-        int r=0;
-        
-        int min=INT_MAX;
-        int max=0;
+        int first = -1;
+        int prev = -1;
+        int minDist = INT_MAX;
+        int maxDist = -1;
 
-        int ns=chk.size();
+        for (int i = 0; i < chk.size(); i++) {
+            if (!chk[i]) continue;
 
-        while(r<ns){
-            if(chk[r] && l==-1)l=r;
-            if(chk[r])max=r;
-            r++;
-        }
-        max-=l;
-
-        l=-1;
-        r=0;
-
-        while(r<ns){
-            if(chk[r]&&l==-1){l=r;r++;continue;}
-            if(chk[r]){
-                if((r-l)<min)min=r-l;
+            if (first == -1) {
+                first = i;
+            } else {
+                minDist = min(minDist, i - prev);
+                maxDist = i - first;
             }
-            if(chk[r])l=r;
-            r++;
+
+            prev = i;
         }
-        ans.emplace_back(min);
-        ans.emplace_back(max);
 
+        if (maxDist == -1)
+            return {-1, -1};
 
-        return ans;
+        return {minDist, maxDist};
     }
 };
