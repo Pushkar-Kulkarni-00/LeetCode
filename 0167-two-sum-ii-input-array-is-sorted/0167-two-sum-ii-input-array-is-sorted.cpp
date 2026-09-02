b7bc7787d0ff6aee407753e57580ab8fb3c591fe
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2026-09-02 09:34:01 +0530

    Time: 0 ms (100%), Space: 19.3 MB (99.89%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
-    int bins(vector <int>& k,int t){
-        int l=0;
-        int r=k.size()-1;
-        while(l<=r){
-            int m=(l+r)/2;
-            if(k[m]==t)return m;
-            else if(k[m]<t)l=m+1;
-            else r=m-1;
-        }
-        return -1;
-    }
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int n=numbers.size();
-        for(int i=0;i<n;i++){
-            int nt=target-numbers[i];
-            int ni=bins(numbers,nt);
-            if(ni==-1)continue;
-            if(i==ni)continue;
-            return {(i<ni)?i+1:ni+1,(i>ni)?i+1:ni+1};
+        int r=numbers.size()-1;
+        int l=0;
+        while(l<r){
+            int s=numbers[l]+numbers[r];
+            if(s==target)return {l+1,r+1};
+            if(s>target)r--;
+            else l++;
         }
         return {-1,-1};
     }
