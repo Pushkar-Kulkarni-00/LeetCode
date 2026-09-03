fec1a9b4be567f28f0d92f1300f81dd50c9e2641
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2026-09-03 09:35:05 +0530

    Time: 0 ms (100%), Space: 166 MB (31.94%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        vector <int> t(2,0);
-        for(int x:nums1)t[x&1]++;
-        if(t[0]==0 || t[1]==0)return true;
-        int mino=INT_MAX;
-        for(int x:nums1)if(x&1)if(x<mino)mino=x;
-        for(int x:nums1)if(x%2==0)if((x-mino)<1)return false;
-        return true;
+        int m=INT_MAX;
+        for(int x:nums1)if(x<m)m=x;
+        if(m&1)return 1;
+        for(int x:nums1)if(x&1)return 0;
+        return 1;
     }
 };
