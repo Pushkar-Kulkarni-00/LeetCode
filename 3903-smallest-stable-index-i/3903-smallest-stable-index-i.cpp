c6f8e11c8587f8808746116ac60aaa663f79ce99
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2026-09-04 08:42:22 +0530

    Time: 6 ms (5.31%), Space: 32 MB (5.99%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -8,14 +8,11 @@ public:
         for(int i=1;i<n;i++)max[i]=(max[i-1]>nums[i])?max[i-1]:nums[i];
         min[n-1]=nums[n-1];
         for(int i=n-2;i>=0;i--)min[i]=(min[i+1]<nums[i])?min[i+1]:nums[i];
-
         vector <int> t;
         for(int i=0;i<n;i++)t.emplace_back(max[i]-min[i]);
-
         for(int i=0;i<n;i++){
             if(t[i]<=k)return i;
-        }
-        
+        }    
         return -1;
     }
 };
