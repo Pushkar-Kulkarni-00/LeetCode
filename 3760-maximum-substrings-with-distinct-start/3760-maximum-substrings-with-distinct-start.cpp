fb93269218c0b6011b300fe161c1c4ad5fe670b7
Pushkar Kulkarni <222581946+Pushkar-Kulkarni-00@users.noreply.github.com>
2026-09-09 11:18:19 +0530

    Time: 18 ms (68.75%), Space: 23.9 MB (71.94%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int maxDistinct(string s) {
-        vector <int> f(26,0);
+        vector <bool> f(26,0);
         int ans=0;
-        for(char x:s)f[x-'a']++;
+        for(char x:s)f[x-'a']=true;
         for(int x:f)if(x)ans++;
         return ans;
     }
